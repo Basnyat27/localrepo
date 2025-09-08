e65c47437a59b6a4d0a24046d195b58ba8c708c1
Basnyat27 <rockeyman2711@gmail.com>
2025-09-08 14:44:58 +0545

    add python solution

---
+++ LocalRepo/PySoln.py
@@ -0,0 +1,33 @@
+# # ---------------------------------------------------------------
+# # Ans.1
+# str = "aaabbccccdee"
+
+# index=0
+# count=1
+
+# while index < len(str)-1:
+#     if str[index] != str[index+1]:
+#         print(str[index], count, sep="", end="")
+#         count = 1
+#     else:
+#         count += 1
+#     index += 1
+# print(str[-1], count, sep="", end="")
+
+
+# # ----------------------------------------------------------------
+# # Ans.2
+# list=[0, 1, 0, 3, 12]
+
+# index = 0
+# while index <= len(list)-1:
+#     if list[index] == 0:
+#         list.pop(index)
+#         list.append(0)
+#     index +=1
+
+# print(list)
+# print (len(list))
+
+
+# Ans.3
